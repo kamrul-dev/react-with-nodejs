67581ce493102027075c7490dd75375434e1e587
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-19 22:35:35 +0600

    Create React form and Post API and send data to the server

--- src/App.js
+++ src/App.js
@@ -7,12 +7,38 @@ function App() {
 
   useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
+      .then(res => res.json())
+      .then(data => setUsers(data))
   }, [])
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    // post data to server
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
   return (
     <div className="App">
       <h1>My own Data : {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder='Name' required />
+        <input type="text" name="email" placeholder='email' required />
+        <input type="submit" value="Add User" />
+      </form>
       <ul>
         {
           users.map(user => <li key={user.id}>id: {user.id} Name:{user.name} email: {user.email}</li>)
